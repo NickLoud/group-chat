972fe2ce8c089736b423a15e4a2d88ae6d7de917
NickLoud <vozisovnikita@gmail.com>
2018-07-27 02:10:58 +0300

    [Add] nicknames

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+    socket.on('chat message', function(nick, msg){
+        io.emit('chat message', nick,msg);
     });
 });
 
