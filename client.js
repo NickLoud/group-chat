46327046db641532b557eb217be39883e73af30d
NickLoud <vozisovnikita@gmail.com>
2018-07-27 22:55:57 +0300

    [Refactor] arrow functions

--- client.js
+++ client.js
@@ -1,19 +1,19 @@
 import io from 'socket.io-client'
-(function() {
+
+(() => {
     let socket = io();
     let form = document.getElementById("form");
     let nickname = document.getElementById("nickname");
     let message = document.getElementById("message");
     let messages = document.getElementById("messages");
-    form.onsubmit = function () {
+    form.onsubmit = () => {
         socket.emit('chat message', nickname.value, message.value);
-        message.value="";
+        message.value = "";
         return false;
     };
-    socket.on('chat message',function (nickname,message) {
-        console.log(nickname);
-        let li=document.createElement("li");
-        li.textContent=`${nickname}: ${message}`;
+    socket.on('chat message', (nickname, message) => {
+        let li = document.createElement("li");
+        li.textContent = `${nickname}: ${message}`;
         messages.appendChild(li);
     });
 })();
--- index.js
+++ index.js
@@ -5,17 +5,17 @@ let io = require('socket.io')(http);
 
 app.use('/public', express.static('public'));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     //res.writeHead(200, {"Content-Type": "text/plain"});
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(nick, msg){
-        io.emit('chat message', nick,msg);
+io.on('connection', (socket) => {
+    socket.on('chat message', function (nick, msg) {
+        io.emit('chat message', nick, msg);
     });
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
