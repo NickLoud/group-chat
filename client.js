2e024299a837d6e3939f1db778a3b9140536a9fe
NickLoud <vozisovnikita@gmail.com>
2018-07-31 12:41:09 +0300

    [Add] Modal window * Nickname input has been removed from bottom * Now modal window opens, when page loads. Here nickname can be entered

--- client.js
+++ client.js
@@ -3,22 +3,30 @@ import io from 'socket.io-client'
 (() => {
     let socket = io();
     let form = document.getElementsByClassName("main__form")[0];
-    let nickname = document.getElementsByClassName("main__nickname")[0];
     let message = document.getElementsByClassName("main__message")[0];
     let messages = document.getElementsByClassName("main__messages")[0];
     let nick;
+    let nicknameButton = document.getElementsByClassName("main__nicknameButton")[0];
+
+    nicknameButton.onclick=()=>{
+        let nicknameDialog=document.getElementsByClassName("main__blackout")[0];
+        let nicknameInput=document.getElementsByClassName("main__nicknameInput")[0];
+        nick=nicknameInput.value;
+        nicknameDialog.style.display="none";
+    };
+
     form.onsubmit = () => {
-        socket.emit('chat message', nickname.value, message.value);
+        socket.emit('chat message', nick, message.value);
         message.value = "";
-        nick=nickname.value;
+        debugger;
         return false;
     };
+
     socket.on('chat message', (nickname, message) => {
         let div = document.createElement("div");
         let divWrapper= document.createElement("div");
         divWrapper.classList.add("message");
         div.classList.add("messageContent");
-        debugger;
         if (nickname === nick){
             div.classList.add("myMessage");
         }
